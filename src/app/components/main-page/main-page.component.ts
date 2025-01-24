e94e73b828ccb67ceeea3c00852970f768331096
matheus duarte <matheus.duarte@everondata.com.br>
2025-01-24 13:59:36 -0300

    adição de novas fotos e correção de erro ao emcaminhar para o wpp

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -16,4 +16,9 @@ export class MainPageComponent {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   }
+
+  goToWhatsApp(): void {
+    const whatsappUrl = 'https://wa.me/5583999083985?text=Ol%C3%A1%20Guedes%2C%20tudo%20bem%3F%20Gostaria%20de%20fazer%20um%20or%C3%A7amento!';
+    window.location.href = whatsappUrl;
+  }
 }
